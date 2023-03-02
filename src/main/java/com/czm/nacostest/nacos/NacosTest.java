9b7f84b7eac1aee051cce9fb7ad735f8a9e5421a
czm <1034946681@qq.com>
2023-03-02 21:25:26 +0800

    变更

--- src/main/java/com/czm/nacostest/nacos/NacosTest.java
+++ src/main/java/com/czm/nacostest/nacos/NacosTest.java
@@ -1,9 +1,32 @@
-package com.czm.nacostest.nacos;/**
-* @author: caozm
-* @create: 2023-03-01 23:33
-* @Version 1.0
-* @description: 
-**/
+package com.czm.nacostest.nacos;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: caozm
+ * @create: 2023-03-01 23:33
+ * @Version 1.0
+ * @description:
+ **/
+@RestController
+@RefreshScope
 public class NacosTest {
 
+    @Value("${czm.user_name}")
+    private String userName;
+
+    @Value("${test.timeout}")
+    private String timeOut;
+
+
+    @GetMapping("/user")
+    public String test() {
+        System.out.println(userName);
+        return userName + "-----" + timeOut;
+    }
 }
